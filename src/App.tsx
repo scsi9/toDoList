2d317f253954c88fe7e518dc3cd953cbc1eab302
scsi9 <scsi8@mail.ru>
2023-11-30 17:02:16 +0300

    firstLesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Todolist } from "./Todolist";
 
 function App() {
+  let tasks1 = [
+    { id: 1, title: "CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false },
+  ];
+
+  let tasks2 = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 2, title: "XXX", isDone: false },
+    { id: 3, title: "Jentelments of fortune", isDone: true },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="Movies" tasks={tasks2} />
     </div>
   );
 }
